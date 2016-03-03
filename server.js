c2b2905de2ac468c4775d1e9b1a50d99cf86d429
Michael Conrad <michael@threepears.com>
2016-03-03 10:45:22 -0600

    Add public folder, add folder to gitignore, create basic socket connection

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 "use strict";
 
-const app = require("express")();
+const express = require("express");
+const app = express();
 const server = require("http").createServer(app);
-const io = require("socket.io")(server);
+const ws = require("socket.io")(server);
 
 const PORT = process.env.PORT || 3000;
 
+app.set("view engine", "jade");
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.render("index");
 });
 
 server.listen(PORT, () => {
   console.log(`Server listening on PORT ${PORT}`)
+});
+
+ws.on("connection", socket => {
+  console.log("socket connected");
 });
