fd9eb90dee0851f8384d4f0c3a00fa921d8fe4dd
Michael Conrad <michael@threepears.com>
2016-03-03 14:48:02 -0600

    Add target ul, create list update functionality, make db and save one row

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,66 @@
-io.connect()
+;(function () {
+  "use strict";
+
+  const ws = io.connect();
+
+  ws.on("connect", () => {
+    console.log("socket connected");
+  });
+
+  ws.on('receiveChat', msg => {
+    displayChat(msg);
+  });
+
+  const form = document.querySelector('form');
+  const name = document.querySelector('input[name="name"]');
+  const text = document.querySelector('input[name="text"]');
+  const ul = document.querySelector('ul');
+
+  form.addEventListener('submit', () => {
+    const chat = {
+      name: name.value,
+      text: text.value
+    };
+
+    ws.emit('sendChat', chat);
+
+    displayChat(chat);
+
+    text.value = '';
+
+    event.preventDefault();
+  });
+
+  function displayChat (chat) {
+    const li = generateLI(chat.name, chat.text);
+
+    ul.appendChild(li);
+  };
+
+  function generateLI (name, text) {
+    const li = document.createElement('li');
+    const textNode = document.createTextNode(`${name}: ${text}`);
+
+    li.appendChild(textNode);
+    return li;
+  };
+
+  function getJSON(url, cb) {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', url);
+
+    request.onload = () => {
+      cb(JSON.parse(request.responseText));
+    };
+
+    request.send();
+  };
+
+  document.addEventListener("DOMContentLoaded", () => {
+    getJSON("/chats", chats => {
+      chats.forEach(displayChat);
+    });
+  });
+
+}());
--- server.js
+++ server.js
@@ -2,10 +2,14 @@
 
 const express = require("express");
 const app = express();
+const pg = require("pg");
 const server = require("http").createServer(app);
 const ws = require("socket.io")(server);
 
 const PORT = process.env.PORT || 3000;
+const POSTGRES_URL = process.env.POSTGRES_URL || 'postgres://localhost:5432/nodechat';
+
+const db = new pg.Client(POSTGRES_URL);
 
 app.set("view engine", "jade");
 
@@ -15,10 +19,27 @@ app.get("/", (req, res) => {
   res.render("index");
 });
 
-server.listen(PORT, () => {
-  console.log(`Server listening on PORT ${PORT}`)
+app.get('/chats', (req, res) => {
+  db.query('SELECT * FROM chats', (err, result) => {
+    if (err) throw err;
+
+    res.send(result.rows);
+  });
 });
+
+db.connect((err) => {
+  if (err) throw err
+
+  server.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`)
+  })
+})
 
 ws.on("connection", socket => {
   console.log("socket connected");
+
+  socket.on('sendChat', msg => {
+    console.log(msg);
+    socket.broadcast.emit('receiveChat', msg);
+  });
 });
